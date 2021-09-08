9459a9a52ad6201eed97ccce7d8004b154d82777
WilmerRS <estwilmerrodriguez@gmail.com>
2021-09-08 14:21:45 -0500

    [fix] Fix url vortal Unipamplona

--- js/background.js
+++ js/background.js
@@ -7,7 +7,7 @@ chrome.runtime.onInstalled.addListener(function () {
      chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
         chrome.declarativeContent.onPageChanged.addRules([{
             conditions: [new chrome.declarativeContent.PageStateMatcher({
-                pageUrl: { hostEquals: 'vortal.unipamplona.edu.co' },
+                pageUrl: { hostEquals: 'hermesoft.unipamplona.edu.co' },
             })],
             actions: [new chrome.declarativeContent.ShowPageAction()]
         }]);
